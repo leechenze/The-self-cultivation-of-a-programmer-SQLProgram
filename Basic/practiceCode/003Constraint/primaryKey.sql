d8f3babb19af56f68e86ac3b6773ea7a6fe0546a
leechenze <leeczyc@gmail.com>
2024-12-30 21:19:06 +0800

    P033T000000

--- Basic/practiceCode/003Constraint/primaryKey.sql
+++ Basic/practiceCode/003Constraint/primaryKey.sql
@@ -5,13 +5,6 @@
 
 
 
-
-
-
-
-
-
-
 ##############################
 ########## 单列主键 ###########
 ##############################
@@ -52,8 +45,42 @@
 ###### 多列主键/复合主键 #######
 ##############################
 
-# TODO
+create table emp2
+(
+    name   varchar(20),
+    deptId int,
+    salary double,
+    constraint pk2 primary key (name, deptId)
+);
 
+
+
+##############################
+###### 修改表结构/添加主键 ######
+##############################
+create table emp3
+(
+    eid    int,
+    name   varchar(20),
+    deptId int,
+    salary double
+);
+alter table emp3
+    add primary key (eid);
+alter table emp3
+    add primary key (eid, deptId);
+
+##############################
+###### 修改表结构/删除主键 ######
+##############################
+alter table emp3 drop primary key;
+
+show columns from emp3;
+# 等价于
+describe emp3;
+
+# 修改表name字段允许为null
+alter table emp3 modify name varchar(20) null;
 
 
 
